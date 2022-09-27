98d870f26a37140897b5eff60d54240cb57a1057
hasibferdous <hasibferdous.ict@gmail.com>
2022-09-27 14:32:55 +0600

    Calculate shopping cart price, shipping and tax with toFixed

--- src/Component/Product/Product.js
+++ src/Component/Product/Product.js
@@ -1,3 +1,5 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import React from 'react';
 import './Product.css'
 
@@ -16,7 +18,8 @@ const Product = (props) => {
             <p><small>Ratings:{ratings}</small></p>
           </div>
          <button onClick={()=>props.handleAddToCart(props.product)} className='btn-cart'>
-            <p>Add to Cart</p>
+            <p className='btn-text'>Add to Cart</p>
+            <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
          </button>
         </div>
     );
