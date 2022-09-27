c34fe7cb2571d45de6c517e33d6fa77d5d3c6c6b
hasibferdous <hasibferdous.ict@gmail.com>
2022-09-27 21:04:01 +0600

    local storage

--- src/Component/Shop/Shop.js
+++ src/Component/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getStoredCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import'./Shop.css'
@@ -12,10 +13,29 @@ const Shop = () => {
         .then(data =>  setProducts(data))
     }, []);
 
+    useEffect(() =>{
+        console.log('local Storage first line', products)
+        const storedCart = getStoredCart();
+        const savedCart = [];
+        for(const id in storedCart){
+            const addedProduct = products.find(product=> product.id === id);
+            if(addedProduct){
+                const quantity = storedCart[id];
+                addedProduct.quantity =quantity;
+                savedCart.push(addedProduct);
+            
+            
+            //console.log(addedProduct);
+            }
+        }
+        setCart(savedCart);
+
+    }, [products])
     const handleAddToCart = (product) =>{
         console.log(product);
         const newCart = [...cart, product];
         setCart(newCart);
+        addToDb(product.id)
     }
     
     return (
